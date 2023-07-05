1dc3ed184a892994bce7bf243aecaf239044be9a
kayisha Ulysse <kayishaulysse@kayishas-MBP.fios-router.home>
2023-07-05 18:32:19 -0400

    Navbar editing

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,7 +6,7 @@ const Footer = (props) => {
         <div className="py-8 flex items-center justify-between">
             <span>{new Date().getFullYear()} &copy; All Right Reserved.</span>
             <div className="flex items-center">
-                Checkout What I'm Building in Web3! <span className="text-primary text-2xl px-1">&#9825;</span>
+                Checkout What We're Building in Web3! <span className="text-primary text-2xl px-1">&#9825;</span>
                 <a href="https://twitter.com/litter_token" className="underline underline-offset-2" target={"_blank"}>Litter Token</a>
             </div>
            
