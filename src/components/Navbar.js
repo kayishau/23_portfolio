78ea5305316183fb405af94624df14d3cd7241f1
kayisha Ulysse <kayishaulysse@kayishas-MBP.fios-router.home>
2023-07-05 21:42:42 -0400

    Icon svgs imported

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,19 +1,33 @@
 import React from 'react'
+import { motion } from 'framer-motion'
 
 
 const NavBar = () => {
 
     return (
         <>
-        <header className='w-full px-32 py-8 font-medium flex items-center justify-between'> 
+        <header className='w-full px-32 py-8 font-medium flex justify-between items-center'> 
             <nav>
-
+                <a href=''>HOME</a>
+                <a href=''>ABOUT</a>
+                <a href=''>PROJECTS</a>
+                <a href=''>CONTACT ME</a>
+            </nav>
+            <nav>
+            <motion.a href="https://www.linkedin.com/in/kayisha-ulysse/" target={"_blank"} className="w-6 ml-3"
+                 whileHover={{y:-2}}
+                 whileTap={{scale:0.9}}>
+                    {/* <LinkedInIcon /> */}
+                    LinkedInIcon
+                </motion.a>
+                <a href=''>Twitter</a>
+                <a href=''>Github</a>
             </nav>
         <h1 className='text-primary'>NavBar</h1>
         </header>
     
         </>
     )
-
 }
 export default NavBar
+
