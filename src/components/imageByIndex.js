c66c98f43304a6e1d2754711c90ab78158c1ce0f
kayishau <ulyssekayisha@gmail.com>
2023-08-17 00:41:26 -0400

    subbing the projects component instead of image-test

--- src/components/imageByIndex.js
+++ src/components/imageByIndex.js
@@ -1,9 +1,15 @@
-import image1 from '../imgs/testimage.JPG'
-import image2 from '../imgs/testimage.JPG'
-import image3 from '../imgs/testimage.JPG'
-import image4 from '../imgs/testimage.JPG'
+// import image1 from '../imgs/testimage.JPG'
+// import image2 from '../imgs/testimage.JPG'
+// import image3 from '../imgs/testimage.JPG'
+// import image4 from '../imgs/testimage.JPG'
+import projectsComponent1 from '../projects/firstProject'
+import projectsComponent2 from '../projects/firstProject'
+import projectsComponent3 from '../projects/firstProject'
+import projectsComponent4 from '../projects/firstProject'
 
-export const images = [image1, image2, image3, image4]
+// export const images = [image1, image2, image3, image4]
+
+export const images = [projectsComponent1, projectsComponent2, projectsComponent3, projectsComponent4]
 
 const imageByIndex = (index) => images[index % images.length]
 
