655e969360e926185873f15e17711bd3cc6e504e
kayishau <ulyssekayisha@gmail.com>
2023-08-22 03:05:17 -0400

    skills section complete

--- src/sections/Skills.js
+++ src/sections/Skills.js
@@ -1,13 +1,20 @@
-import { FaReact, FaNodeJs, FaRust, FaPython, FaJira } from 'react-icons/fa'
-import { SiNextdotjs, SiCanva, SiAdobe, SiMongodb, SiTailwindcss, SiJest, SiPostman, SiTrello, SiExpress } from 'react-icons/si'
-import { FiFigma } from 'react-icons/fi'
-import { RiJavascriptLine, RiBootstrapLine } from 'react-icons/ri'
-import { BiLogoPostgresql } from 'react-icons/bi'
-import { TbBrandDjango, TbBrandFramerMotion } from 'react-icons/tb'
-
-
-
-import { motion, Variants } from "framer-motion";
+import { FaReact, FaNodeJs, FaRust, FaPython, FaJira } from "react-icons/fa";
+import {
+  SiNextdotjs,
+  SiCanva,
+  SiAdobe,
+  SiMongodb,
+  SiTailwindcss,
+  SiJest,
+  SiPostman,
+  SiTrello,
+  SiExpress,
+} from "react-icons/si";
+import { FiFigma } from "react-icons/fi";
+import { RiJavascriptLine, RiBootstrapLine } from "react-icons/ri";
+import { BiLogoPostgresql } from "react-icons/bi";
+import { TbBrandDjango, TbBrandFramerMotion } from "react-icons/tb";
+import { motion } from "framer-motion";
 
 export default function Skills() {
   // useEffect(() => {
@@ -23,74 +30,124 @@ export default function Skills() {
           backgroundSize: "cover",
           backgroundRepeat: "no-repeat",
           backgroundPosition: "bottom",
-         
         }}
       >
-        <div className="w-10/12 h-screen gap-10 flex flex-col justify-center border border-green-700"
-        >
-        <h1 className="font-cg text-4xl text-center border border-black font-extrabold mt-10">Skills :)</h1>
-        <div className="flex text-center justify-around border border-black">
+        <div className="w-10/12 flex flex-col">
+          <h1 className="font-cg text-4xl text-center font-extrabold mt-10">
+            Skills :)
+          </h1>
+          <p className="font-cg text-xl text-center">
+            “Improve by 1% a day, and in just 70 days, you’re twice as good.” –
+            Alan Weiss
+          </p>
+          <div className="flex">
+            <div
+              className="flex flex-wrap w-1/3 justify-center items-center"
+              style={{
+                backgroundImage: "url(/img/front.png)",
+                backgroundSize: "contain",
+                backgroundRepeat: "no-repeat",
+                height: "500px",
+                backgroundPosition: "center",
+              }}
+            >
+              <div className="flex flex-wrap gap-5 justify-center px-5">
+                <motion.div whileHover={{ y: -5 }} whileTap={{ scale: 0.9 }}>
+                  <FaReact className="w-11 h-11" />
+                </motion.div>
+                <motion.div whileHover={{ y: -5 }} whileTap={{ scale: 0.9 }}>
+                  <SiNextdotjs className="w-11 h-11" />
+                </motion.div>
+                <motion.div whileHover={{ y: -5 }} whileTap={{ scale: 0.9 }}>
+                  <FiFigma className="w-11 h-11" />
+                </motion.div>
+                <motion.div whileHover={{ y: -5 }} whileTap={{ scale: 0.9 }}>
+                  <SiCanva className="w-11 h-11" />
+                </motion.div>
+                <motion.div whileHover={{ y: -5 }} whileTap={{ scale: 0.9 }}>
+                  <SiAdobe className="w-11 h-11" />
+                </motion.div>
+                <motion.div whileHover={{ y: -5 }} whileTap={{ scale: 0.9 }}>
+                  <RiBootstrapLine className="w-11 h-11" />
+                </motion.div>
+                <motion.div whileHover={{ y: -5 }} whileTap={{ scale: 0.9 }}>
+                  <SiTailwindcss className="w-11 h-11" />
+                </motion.div>
+                <motion.div whileHover={{ y: -5 }} whileTap={{ scale: 0.9 }}>
+                  <TbBrandFramerMotion className="w-11 h-11" />
+                </motion.div>
+              </div>
+            </div>
 
-          <div className="w-1/3"
-           style={{
-            backgroundImage: "url(/img/front.png)",
-            backgroundSize: "contain",
-            backgroundRepeat: "no-repeat",
-            height: "500px"
-          }}
-          >
-          <div className='flex flex-wrap justify-center gap-5 border border-green-700 px-5'>
-          <FaReact className='w-11 h-11'/>
-          <SiNextdotjs className='w-11 h-11'/>
-          <FiFigma className='w-11 h-11'/>
-          <SiCanva className='w-11 h-11'/>
-          <SiAdobe className='w-11 h-11'/>
-          <RiBootstrapLine className='w-11 h-11'/>
-          <SiTailwindcss className='w-11 h-11'/>
-          <TbBrandFramerMotion className='w-11 h-11'/>
-          </div>
-          </div>
-
-          <div className="w-1/3"
+            <div
+              className="flex flex-wrap w-1/3 justify-center items-center"
               style={{
                 backgroundImage: "url(/img/languages.png)",
                 backgroundSize: "contain",
                 backgroundRepeat: "no-repeat",
-                height: "500px"
+                height: "500px",
+                backgroundPosition: "center",
               }}
-          >
-          <div className='flex flex-wrap justify-center gap-5 border border-green-700 px-5'>
-            <FaRust className='w-11 h-11'/>
-            <FaPython className='w-11 h-11'/>
-            <RiJavascriptLine className='w-11 h-11'/>
-          </div>
-          </div>
+            >
+              <div className="flex flex-wrap gap-5 justify-center">
+                <motion.div whileHover={{ y: -5 }} whileTap={{ scale: 0.9 }}>
+                  <FaRust className="w-11 h-11" />
+                </motion.div>
+                <motion.div whileHover={{ y: -5 }} whileTap={{ scale: 0.9 }}>
+                  <FaPython className="w-11 h-11" />
+                </motion.div>
+                <motion.div whileHover={{ y: -5 }} whileTap={{ scale: 0.9 }}>
+                  <RiJavascriptLine className="w-11 h-11" />
+                </motion.div>
+              </div>
+            </div>
 
-          <div className="w-1/3"
+            <div
+              className="flex flex-wrap w-1/3 justify-center items-center"
               style={{
                 backgroundImage: "url(/img/back.png)",
                 backgroundSize: "contain",
                 backgroundRepeat: "no-repeat",
-                height: "500px"
+                height: "500px",
+                backgroundPosition: "center",
               }}
-          >
-          <div className='flex flex-wrap justify-center gap-5 border border-green-700 px-5'>
-          <FaNodeJs className='w-11 h-11'/>
-          <BiLogoPostgresql className='w-11 h-11'/>
-          <TbBrandDjango className='w-11 h-11'/>
-          <SiMongodb className='w-11 h-11'/>
-          <SiJest className='w-11 h-11'/>
-          <SiPostman className='w-11 h-11'/>
-          <SiTrello className='w-11 h-11'/>
-          <FaJira className='w-11 h-11'/>
-          <SiExpress className='w-11 h-11'/>
+            >
+              <div className="flex flex-wrap gap-5 justify-center px-5">
+                <motion.div whileHover={{ y: -5 }} whileTap={{ scale: 0.9 }}>
+                  <FaNodeJs className="w-11 h-11" />
+                </motion.div>
+                <motion.div whileHover={{ y: -5 }} whileTap={{ scale: 0.9 }}>
+                  <BiLogoPostgresql className="w-11 h-11" />
+                </motion.div>
+                <motion.div whileHover={{ y: -5 }} whileTap={{ scale: 0.9 }}>
+                  <TbBrandDjango className="w-11 h-11" />
+                </motion.div>
+                <motion.div whileHover={{ y: -5 }} whileTap={{ scale: 0.9 }}>
+                  <SiMongodb className="w-11 h-11" />
+                </motion.div>
+                <motion.div whileHover={{ y: -5 }} whileTap={{ scale: 0.9 }}>
+                  <SiJest className="w-11 h-11" />
+                </motion.div>
+                <motion.div whileHover={{ y: -5 }} whileTap={{ scale: 0.9 }}>
+                  <SiPostman className="w-11 h-11" />
+                </motion.div>
+                <motion.div whileHover={{ y: -5 }} whileTap={{ scale: 0.9 }}>
+                  <SiTrello className="w-11 h-11" />
+                </motion.div>
+                <motion.div whileHover={{ y: -5 }} whileTap={{ scale: 0.9 }}>
+                  <FaJira className="w-11 h-11" />
+                </motion.div>
+
+                <motion.div whileHover={{ y: -5 }} whileTap={{ scale: 0.9 }}>
+                  <SiExpress className="w-11 h-11" />
+                </motion.div>
+              </div>
+            </div>
           </div>
-          </div>
-        </div>
         </div>
         <div className=" font-cg self-end mb-10 mr-6">
-              <h1 className="text-black text-4xl self-end">3</h1>
-            </div>
+          <h1 className="text-black text-4xl self-end">3</h1>
+        </div>
       </section>
     </>
   );
