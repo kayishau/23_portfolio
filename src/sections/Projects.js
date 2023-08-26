d28030591181f404fbabdfd11c6f43ac07888d7c
kayishau <ulyssekayisha@gmail.com>
2023-08-25 20:49:55 -0400

    edits

--- src/sections/Projects.js
+++ src/sections/Projects.js
@@ -2,6 +2,7 @@ import React from "react";
 import EmblaCarousel from "../components/EmblaCarousel";
 import { projectsArray } from "../components/imageByIndex";
 import "../index.css";
+import { RiArrowRightDoubleLine, RiArrowLeftDoubleFill } from "react-icons/ri";
 
 const OPTIONS = { loop: true };
 // const SLIDE_COUNT = 5
@@ -14,8 +15,18 @@ export default function Projects() {
         id="projectsSection"
         className="w-full min-h-screen flex flex-col justify-center border-t border-slate-500"
       >
-        {/* <h1 className="font-cg text-4xl font-extrabold mt-10">Creatively Coding :)</h1> */}
-        <section className="justify-center flex flex-col my-5">
+        <div className="flex justify-center items-center gap-5 mb-2 tablet:mb-4">
+         <div>
+         <RiArrowLeftDoubleFill className="w-5 h-5 "/>
+          </div> 
+          <div>
+          <h1 className="font-cg text-2xl lg:text-3xl xl:text-4xl text-center font-extrabold">Swipe :)</h1>
+          </div>
+        <div>
+         <RiArrowRightDoubleLine className="w-5 h-5 "/>
+          </div> 
+        </div>
+        <section className="justify-center flex flex-col md:my-5">
           <EmblaCarousel slides={SLIDES} options={OPTIONS} />
           <div className="self-end md:pb-12 bg-blue-500 mr-5">
             <h1 className="font-cg text-black text-sm tablet:text-lg md:text-xl lg:text-2xl">
