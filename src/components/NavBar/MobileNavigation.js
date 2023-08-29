5a09235d98a565335e418f8f874be42f0043cf8b
kayishau <ulyssekayisha@gmail.com>
2023-08-28 20:52:30 -0400

    switched mobile nav to right, orange

--- src/components/NavBar/MobileNavigation.js
+++ src/components/NavBar/MobileNavigation.js
@@ -9,10 +9,10 @@ const MobileNavigation = () => {
 
     const [openBurger, setOpenBurger] = useState(false)
 
-    const hamburgerIcon = <GiHamburgerMenu className="w-6 h-6 absolute left-4 top-3 cursor-pointer "
+    const hamburgerIcon = <GiHamburgerMenu className="w-6 h-6 right-5 top-3 absolute cursor-pointer text-primary"
     onClick={() => setOpenBurger(!openBurger)}/>
 
-    const closeIcon = <AiOutlineCloseSquare className="w-7 h-7 absolute left-4 top-3 hover:text-white duration-150 ease-in-out cursor-pointer"
+    const closeIcon = <AiOutlineCloseSquare className="w-7 h-7 absolute right-5 top-3 hover:text-white duration-150 ease-in-out cursor-pointer"
     onClick={() => setOpenBurger(!openBurger)}/>
 
     const closeMobileMenu = () => setOpenBurger(false)
@@ -22,7 +22,7 @@ const MobileNavigation = () => {
     <div
     //  className="w-full h-[50px] bg-primary/50 fixed"
      >
-    <nav className="tablet:hidden fixed w-full flex top-6 justify-center duration-150 ease-in-out"
+    <nav className="tablet:hidden fixed w-full justify-center flex duration-150 ease-in-out"
       style={{
         backgroundImage: "url(/img/navbar5.png)",
         backgroundSize: "cover",
